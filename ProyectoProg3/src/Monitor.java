fc98313b05f32a19c7d86c9518e4c676dc66df07
AlexUP27 <alex.u.p@opendeusto.es>
2024-10-25 12:23:59 +0200

    Meter

--- ProyectoProg3/src/Monitor.java
+++ ProyectoProg3/src/Monitor.java
@@ -1,5 +1,33 @@
 
 public class Monitor extends Persona{
 	TipoEspecialidad especialidad;
+
+	public Monitor() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Monitor(String nombre, String telefono, String email, String dir) {
+		super(nombre, telefono, email, dir);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Monitor(TipoEspecialidad especialidad) {
+		super();
+		this.especialidad = especialidad;
+	}
+
+	public TipoEspecialidad getEspecialidad() {
+		return especialidad;
+	}
+
+	public void setEspecialidad(TipoEspecialidad especialidad) {
+		this.especialidad = especialidad;
+	}
+
+	@Override
+	public String toString() {
+		return "Monitor [especialidad=" + especialidad + "]";
+	}
 	
 }
--- ProyectoProg3/src/Usuario.java
+++ ProyectoProg3/src/Usuario.java
@@ -1,4 +1,33 @@
 
 public class Usuario extends Persona{
+	String tipoDeMenbresia;
+
+	public Usuario() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Usuario(String nombre, String telefono, String email, String dir) {
+		super(nombre, telefono, email, dir);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Usuario(String tipoDeMenbresia) {
+		super();
+		this.tipoDeMenbresia = tipoDeMenbresia;
+	}
+
+	public String getTipoDeMenbresia() {
+		return tipoDeMenbresia;
+	}
+
+	public void setTipoDeMenbresia(String tipoDeMenbresia) {
+		this.tipoDeMenbresia = tipoDeMenbresia;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [tipoDeMenbresia=" + tipoDeMenbresia + "]";
+	}
 	
 }
